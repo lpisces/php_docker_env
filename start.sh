ce9b47f4b89bee274f01f5be20281728eee2b003
zebrapool <iamalazyrat@Gmail.com>
2018-05-10 10:45:13 +0800

    fixed

--- start.sh
+++ start.sh
@@ -2,9 +2,7 @@
 
 ./env.py
 
-docker-compose build 
-
-docker-compose up -d --scale php-fpm=3 --scale mysql=2 --scale php_cli_worker=3
+docker-compose up --remove-orphans --build -d --scale php-fpm=3 --scale mysql=2 --scale php_cli_worker=3 --scale redis=2
 
 cp ./www/index.php ../www/
 

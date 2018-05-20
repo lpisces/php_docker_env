60957f4c947ff8f932b90b610c67b3ef8ad5b54e
Sugar <shuwei@nle-tech.com>
2018-05-20 14:20:46 +0800

    fixed

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 ./env.py
 
-docker-compose up --remove-orphans --build -d --scale php-fpm=3 --scale mysql=2 --scale php_cli_worker=3 --scale redis=2
+docker-compose up --remove-orphans --build -d --scale php-fpm=3 --scale mysql=1 --scale php_cli_worker=3 --scale redis=2
 
 cp ./www/index.php ../www/
 

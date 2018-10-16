9eb4b2e8f2c5889554cef7df8ca1621782ae6243
lpisces <iamalazyrat@gmail.com>
2018-10-16 22:36:06 +0800

    fixed

--- env.py
+++ env.py
@@ -3,40 +3,92 @@
 import socket
 import subprocess
 import sys
+import os.path
 
-remoteServer    = "127.0.0.1"
-remoteServerIP  = socket.gethostbyname(remoteServer)
-
-unusedPort = []
-
-for port in range(10000,65535):  
-  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-  result = sock.connect_ex((remoteServerIP, port))
-  if result == 0:
-    sock.close()
+def main():
+  envPath = ".env"
+  if os.path.isfile(envPath):
+    myEnv = readEnvFile(envPath)
   else:
-    unusedPort.append(str(port))
-  if len(unusedPort) == 5:
-      break
+    myEnv = {}
+  
+  startPortFile = "/var/run/docker_compose_start_port"
+  startPort = getStartPort(startPortFile)
+  services = ["NGINX_HTTP", "NGINX_HTTPS", "MYSQL", "PHPMYADMIN", "REDIS"]
+  unusedPort = getUnusedPort(startPort, len(services))
 
-varArray = ["NGINX_HTTP_PORT", "NGINX_HTTPS_PORT", "MYSQL_PORT", "PHPMYADMIN_PORT", "REDIS_PORT"]
+  
+  if len(myEnv) == 0:
+    for i in range(len(services)):
+      myEnv[services[i] + "_PORT"] = unusedPort[i]
 
-network_name = "php_" + "_".join(unusedPort)
-try:
-  subprocess.call("docker network prune -f ", shell=True)
-  subprocess.call("docker network create -d bridge %s" % network_name, shell=True)
-except:
-  pass
+    network = "_".join(unusedPort)
+    try:
+      subprocess.call("docker network prune -f ", shell=True)
+      subprocess.call("docker network create -d bridge %s" % network, shell=True)
+    except:
+      pass
 
-fd = open("./.env", "w")
-#env_config = []
+    myEnv["USER_NETWORK"] = network
+    myEnv["WEB_ROOT"] = "../www"
 
-for i in range(len(varArray)):
-  row = "%s=%s\n" % (varArray[i], unusedPort[i])
-  fd.write(row)
-  #env_config.append(row)
+    writeEnvFile(envPath, myEnv)
+    setNextStartPort(startPortFile, unusedPort[-1])
 
-#env_config.append("%s=%s" % ("USER_NETWORK", network_name))
-fd.write("%s=%s\n" % ("USER_NETWORK", network_name))
-fd.write("%s=%s\n" % ("WEB_ROOT", "../www"))
-fd.close()
+  for key, value in myEnv.iteritems():
+    print "%s=%s" % (key, value)
+
+def isUsedPort(serverIP, port):
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  result = sock.connect_ex((serverIP, port))
+  return result == 0
+
+def getUnusedPort(start, n):
+  maxPort = 65535
+  if start >= maxPort:
+    return []
+
+  while(start % 100 != 0) :
+    start += 1
+    continue
+
+  server = "127.0.0.1"
+  serverIP = socket.gethostbyname(server)
+
+  unused = []
+  for port in range(start, start + n):
+    if False == isUsedPort(serverIP, port):
+      unused.append(str(port))
+    else:
+      return getUnusedPort(port, n)
+  return unused
+  
+def getStartPort(path):
+  if os.path.isfile(path):
+    fd = open(path, "r")
+    startPort = int(fd.read())
+  else:
+    startPort = 10000
+  return startPort
+
+def setNextStartPort(path, port):
+  fd = open(path, 'w')
+  fd.write(str(port))
+  fd.close()
+  
+def writeEnvFile(path, myEnv):
+  fd = open(path, 'w')
+  for key, value in myEnv.iteritems():
+    fd.write("%s=%s\n" % (key, value))
+  fd.close()
+
+def readEnvFile(path):
+  fd = open(path, 'r')
+  myEnv = {}
+  for line in fd.readlines(): 
+    k, v = line.split("=")
+    myEnv[k.strip()] = v.strip()
+  return myEnv
+
+main()
+
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker-compose up --remove-orphans --build -d --scale php-fpm=1 --scale mysql=1 --scale php_cli_worker=1 --scale redis=1
-
-cp ./www/index.php ../www/
-
-cat .env
